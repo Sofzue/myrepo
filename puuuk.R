39f63f49cf066d734b8bfe3cd8b44bd7298bcccc
Sofzue <zsofia124@gmail.com>
2024-07-02 10:45:18 +0400

    added glm on mutagen

--- puuuk.R
+++ puuuk.R
@@ -2,3 +2,11 @@
 x <- rnorm(1000, 0, 1)
 plot(x)
 
+library(GLMsData)
+data("mutagen")
+
+x <- (mutagen$Dose)^1/3
+y <- mutagen$Colonies
+
+glm_mod <- glm(y~x, family = poisson(link = "log"))
+plot(glm_mod)
